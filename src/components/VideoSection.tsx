50502110956822635c9af2d1c5c47c518445e921
EucTech <euccreative@gmail.com>
2025-06-18 20:47:00 -0400

    added VIDEO

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const VideoSection = () => {
   return (
-    <div className=' bg-[#DC1B79] w-full h-fit px-5 sm:px-20 py-20 flex items-center justify-center'>
-        <div className='h-[26em] w-[100%] md:w-[40em] lg:w-[50em] bg-[#000]'>
-
-        </div>
+    <div className=" bg-[#DC1B79] w-full h-fit px-5 sm:px-20 py-20 flex items-center justify-center">
+      <video
+        className="h-[30em] w-full md:w-[40em] lg:w-[50em] bg-[#000] object-cover"
+        autoPlay
+        loop
+        muted
+        controls
+        playsInline
+      >
+        <source src="/video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
-  )
-}
+  );
+};
 
-export default VideoSection
+export default VideoSection;
